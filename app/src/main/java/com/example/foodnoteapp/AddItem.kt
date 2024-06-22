1169439012cf20d3e837472e3002beba46d8c5b4
Madhumidha-S <madhumidha072005@gmail.com>
2024-06-22 15:08:59 +0530

    Input Data Stored in TXT file

--- app/src/main/java/com/example/foodnoteapp/AddItem.kt
+++ app/src/main/java/com/example/foodnoteapp/AddItem.kt
@@ -1,78 +1,90 @@
 package com.example.foodnoteapp
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
-import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.OutlinedTextFieldDefaults
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.DpOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import org.intellij.lang.annotations.JdkConstants.HorizontalAlignment
+import java.io.File
+import java.io.FileOutputStream
 
 class AddItem : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent() {
-            TextField()
+        setContent {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = Color.White
+            ) {
+                TextField(onSave = { food ->
+                }
+                )
+            }
         }
     }
+}
+fun writeFoodItemToFile(context: Context, food: Food, fileName: String) {
+    val file = File(context.filesDir, fileName)
+    val foodItemString = "${food.name},${food.price.toString()}\n"
+    FileOutputStream(file, true).bufferedWriter().use { writer ->
+        writer.write(foodItemString)
+    }
+    println(file.canonicalPath)
+}
+
+enum class FoodType {
+    Breakfast,
+    Lunch,
+    Dinner,
+    Snacks,
+    Drinks,
+    Others
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextField() {
+fun TextField( onSave: (Food) -> Unit) {
+    val context = LocalContext.current
+    val activity= context as? Activity
+    var isExpanded by remember { mutableStateOf(false) }
     var name by remember { mutableStateOf("") }
     var price by remember { mutableStateOf("") }
-    var isExpanded by remember { mutableStateOf(false) }
-    val list = listOf("Select the Category","Breakfast", "Lunch", "Dinner", "Snacks", "Drinks", "Others")
-    var type by remember { mutableStateOf(list[0]) }
-
-    val context = LocalContext.current
-    val activity = context as? Activity
+    var type by remember { mutableStateOf(FoodType.Others) }
 
     Text(text = "Add Food Items",
         style = MaterialTheme.typography.headlineLarge,
@@ -82,7 +94,7 @@ fun TextField() {
         fontSize = 30.sp,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 20.dp)
+            .padding(top = 40.dp)
     )
     Box(){
 
@@ -98,7 +110,7 @@ fun TextField() {
                 value = name,
                 onValueChange = { name = it },
                 label = { Text("Enter Food Item", color = Color(0xFF74C931)) },
-                colors = TextFieldDefaults.outlinedTextFieldColors(
+                colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color(0xFF74C931),
                     unfocusedBorderColor = Color(0xFF74C931),
                 )
@@ -108,12 +120,11 @@ fun TextField() {
                 value = price,
                 onValueChange = { price = it },
                 label = { Text("Enter Price", color = Color(0xFF74C931)) },
-                colors = TextFieldDefaults.outlinedTextFieldColors(
+                colors = OutlinedTextFieldDefaults.colors(
                     focusedBorderColor = Color(0xFF74C931),
                     unfocusedBorderColor = Color(0xFF74C931),
                 )
             )
-
             ExposedDropdownMenuBox(
                 modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
                 expanded = isExpanded,
@@ -123,25 +134,26 @@ fun TextField() {
             ) {
                 OutlinedTextField(
                     modifier = Modifier.menuAnchor(),
-                    value = type,
+                    value = type.name,
                     onValueChange = {},
                     readOnly = true,
+                    label = {Text(text = "Category", color = Color(0xFF74C931))},
                     trailingIcon = {
                         ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
                     },
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                    colors = OutlinedTextFieldDefaults.colors(
                         focusedBorderColor = Color(0xFF74C931),
                         unfocusedBorderColor = Color(0xFF74C931),
                     )
                 )
                 ExposedDropdownMenu(
                     expanded = isExpanded,
-                    onDismissRequest = { isExpanded = false },
+                    onDismissRequest = { isExpanded = false }
                 ) {
-                    list.forEach { selectionOption ->
+                    FoodType.values().forEach { selectedOption ->
                         DropdownMenuItem(
-                            text = { Text(selectionOption) },
-                            onClick = { type = selectionOption
+                            text = { Text(selectedOption.name) },
+                            onClick = { type = selectedOption
                                 isExpanded = false
                             },
                             contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
@@ -153,13 +165,16 @@ fun TextField() {
                 colors = ButtonDefaults.buttonColors(Color(0xFF74C931)),
                 modifier = Modifier.padding(top = 10.dp),
                 onClick = {
-                    if (name.isNotBlank() && price.isNotBlank()) {
+                    if (name.isNotBlank() && price.isNotBlank() && type.name.isNotBlank()) {
                         val foodItem = Food(
                             name =name,
                             price = price,
                             type = type,
                         )
+                        writeFoodItemToFile(context, foodItem, "Food.txt")
+                        onSave(foodItem)
                     }
+
                     context.startActivity(Intent(context,MainActivity::class.java))
                     activity?.finish()
                 }
@@ -170,19 +185,16 @@ fun TextField() {
     }
 }
 
-@Composable
-fun AddPreview(){
-    TextField()
-}
-
-
-class Food(name: String, price: String, type: String) {
-
-}
+data class Food(
+    val name:String,
+    val price :String,
+    val type : FoodType
+)
 
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
 fun TextFieldPreview() {
-    TextField()
+    TextField { food ->
+    }
 }
--- app/src/main/java/com/example/foodnoteapp/MainActivity.kt
+++ app/src/main/java/com/example/foodnoteapp/MainActivity.kt
@@ -1,35 +1,42 @@
 package com.example.foodnoteapp
 
 import android.app.Activity
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Checkbox
+import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
-import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -39,28 +46,43 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import kotlinx.coroutines.launch
+import java.io.File
 
 class MainActivity : ComponentActivity() {
+    private val fileName = "Food.txt"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val context = LocalContext.current
+            if (!fileExists(context, fileName)) {
+                createFile(context, fileName)
+            }
             Tab()
         }
     }
+}
+private fun fileExists(context: Context, fileName: String): Boolean {
+    val file = File(context.filesDir, fileName)
+    return file.exists()
+}
+
+private fun createFile(context: Context, fileName: String) {
+    val file = File(context.filesDir, fileName)
+    file.createNewFile()
 }
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Tab(){
-    val context = LocalContext.current
-    val activity = context as? Activity
-
+fun Tab(
+){
     Column (modifier = Modifier.padding(PaddingValues(10.dp))){
         val pagerState = rememberPagerState (
             pageCount = { 2 }
@@ -81,7 +103,7 @@ fun Tab(){
             androidx.compose.material3.Tab(
                 modifier= Modifier.background(
                     if (pagerState.currentPage == 0){
-                            Color.White
+                        Color.White
                     }
                     else {
                         Color(0xFF74C931)
@@ -110,7 +132,7 @@ fun Tab(){
                 ),
                 selected = pagerState.currentPage ==1,
                 text = {
-                        Text(text = "Fun", fontSize = 25.sp,)
+                    Text(text = "Fun", fontSize = 25.sp,)
                 },
                 onClick = {
                     coroutineScope.launch {
@@ -125,29 +147,51 @@ fun Tab(){
             state = pagerState,
             userScrollEnabled = false
         ) {
-            if (pagerState.currentPage == 0)
-            {
-                Box(modifier = Modifier
-                    .fillMaxSize()
-                    .padding(14.dp)
-                ) {
-                    FloatingActionButton(
-                        onClick = {
-                                  context.startActivity(Intent(context,AddItem::class.java))
-                                  activity?.finish()
-                        },
-                        Modifier
-                            .background(Color(0xFF74C931), CircleShape,)
-                            .align(Alignment.BottomEnd)
-                            .padding(16.dp),
-                        contentColor = Color(0xFF74C931),
-                        shape = CircleShape,
-                        containerColor = Color.White,
-                    ) {
-                        Icon(imageVector = Icons.Filled.Add, contentDescription = null)
-                    }
-                }
+            if (pagerState.currentPage==0){
+                FAB()
             }
         }
     }
+}
+
+@Composable
+fun FAB(){
+    val context = LocalContext.current
+    val activity=context as? Activity
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color.White),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.Start,
+    ) {
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(14.dp)) {
+            FloatingActionButton(
+                onClick = {
+                    context.startActivity(Intent(context,AddItem::class.java))
+                    activity?.finish()
+                },
+                Modifier
+                    .background(Color(0xFF74C931), CircleShape,)
+                    .align(Alignment.BottomEnd)
+                    .padding(16.dp),
+                contentColor = Color(0xFF74C931),
+                shape = CircleShape,
+                containerColor = Color.White,
+            ) {
+                Icon(imageVector = Icons.Filled.Add, contentDescription = null)
+            }
+        }
+
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun MainPreview() {
+    Tab()
 }
