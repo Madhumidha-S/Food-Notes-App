9bde14e7e7a4f31ee41b689fb194859cce923fb1
Madhumidha-S <madhumidha072005@gmail.com>
2024-06-30 16:34:47 +0530

    Lasy list added

--- app/src/main/java/com/example/foodnoteapp/AddItem.kt
+++ app/src/main/java/com/example/foodnoteapp/AddItem.kt
@@ -14,12 +14,17 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
 import androidx.compose.material3.ExposedDropdownMenuDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
@@ -34,7 +39,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -52,45 +56,71 @@ class AddItem : ComponentActivity() {
                 color = Color.White
             ) {
                 TextField(onSave = { food ->
+
                 })
             }
         }
     }
 }
 
-fun fileWrite(context: Context, food: Food, fileName: String) {
+fun writeFoodItemToFile(context: Context, food: Food, fileName: String) {
     val file = File(context.filesDir, fileName)
-    val foodItemString = "${food.name},${food.price},${food.type}\n"
+    val foodItemString = "${food.name},${food.price.toString()}\n"
     FileOutputStream(file, true).bufferedWriter().use { writer ->
         writer.write(foodItemString)
     }
-    println(file.canonicalPath)
 }
+
+data class Food(
+    val name:String,
+    val price :String,
+    val type : String
+)
 
 val foodTypes = listOf("Breakfast", "Lunch", "Dinner", "Snacks", "Drinks", "Others")
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun TextField(onSave: (Food) -> Unit) {
+fun TextField( onSave: (Food) -> Unit) {
     val context = LocalContext.current
-    val activity = context as? Activity
+    val activity= context as? Activity
     var isExpanded by remember { mutableStateOf(false) }
     var name by remember { mutableStateOf("") }
     var price by remember { mutableStateOf("") }
     var type by remember { mutableStateOf("") }
 
-    Text(
-        text = "Add Food Items",
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        IconButton(
+            onClick = {
+                context.startActivity(Intent(context, MainActivity::class.java))
+                activity?.finish()
+            },
+            modifier = Modifier.align(Alignment.TopStart) // Align to top start
+        ) {
+            Icon(
+                imageVector = Icons.Outlined.ArrowBack,
+                tint = Color(0xFF181818),
+                modifier = Modifier.size(30.dp),
+                contentDescription = "Back"
+            )
+        }
+    }
+
+    Text(text = "Add Food Items",
         style = MaterialTheme.typography.headlineLarge,
-        fontWeight = FontWeight.ExtraBold,
-        color = Color(0xFF74C931),
+        color = Color(0xFF181818),
         textAlign = TextAlign.Center,
         fontSize = 30.sp,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(top = 40.dp)
+            .padding(top = 50.dp)
     )
-    Box() {
+    Box(){
+
         Column(
             modifier = Modifier
                 .padding(PaddingValues(10.dp))
@@ -109,7 +139,7 @@ fun TextField(onSave: (Food) -> Unit) {
                 )
             )
             OutlinedTextField(
-                modifier = Modifier.padding(bottom = 10.dp),
+                modifier = Modifier.padding( bottom = 10.dp),
                 value = price,
                 onValueChange = { price = it },
                 label = { Text("Enter Price", color = Color(0xFF74C931)) },
@@ -121,7 +151,7 @@ fun TextField(onSave: (Food) -> Unit) {
             ExposedDropdownMenuBox(
                 modifier = Modifier.padding(top = 10.dp, bottom = 10.dp),
                 expanded = isExpanded,
-                onExpandedChange = { newValue ->
+                onExpandedChange = {newValue ->
                     isExpanded = newValue
                 }
             ) {
@@ -130,7 +160,7 @@ fun TextField(onSave: (Food) -> Unit) {
                     value = type,
                     onValueChange = {},
                     readOnly = true,
-                    label = { Text(text = "Category", color = Color(0xFF74C931)) },
+                    label = { Text("Enter the Category", color = Color(0xFF74C931)) },
                     trailingIcon = {
                         ExposedDropdownMenuDefaults.TrailingIcon(expanded = isExpanded)
                     },
@@ -146,8 +176,7 @@ fun TextField(onSave: (Food) -> Unit) {
                     foodTypes.forEach { selectedOption ->
                         DropdownMenuItem(
                             text = { Text(selectedOption) },
-                            onClick = {
-                                type = selectedOption
+                            onClick = { type = selectedOption
                                 isExpanded = false
                             },
                             contentPadding = ExposedDropdownMenuDefaults.ItemContentPadding,
@@ -161,15 +190,15 @@ fun TextField(onSave: (Food) -> Unit) {
                 onClick = {
                     if (name.isNotBlank() && price.isNotBlank() && type.isNotBlank()) {
                         val foodItem = Food(
-                            name = name,
+                            name =name,
                             price = price,
                             type = type,
                         )
-                        fileWrite(context, foodItem, "Food.txt")
+                        writeFoodItemToFile(context, foodItem, "Food-Data.txt")
                         onSave(foodItem)
                     }
 
-                    context.startActivity(Intent(context, MainActivity::class.java))
+                    context.startActivity(Intent(context,MainActivity::class.java))
                     activity?.finish()
                 }
             ) {
@@ -178,12 +207,6 @@ fun TextField(onSave: (Food) -> Unit) {
         }
     }
 }
-
-data class Food(
-    val name: String,
-    val price: String,
-    val type: String
-)
 
 @Preview(showBackground = true, showSystemUi = true)
 @Composable
--- app/src/main/java/com/example/foodnoteapp/MainActivity.kt
+++ app/src/main/java/com/example/foodnoteapp/MainActivity.kt
@@ -13,11 +13,11 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.pager.HorizontalPager
@@ -25,6 +25,8 @@ import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material.icons.outlined.Create
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.CheckboxDefaults
 import androidx.compose.material3.FloatingActionButton
@@ -46,7 +48,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -54,20 +55,46 @@ import kotlinx.coroutines.launch
 import java.io.File
 
 class MainActivity : ComponentActivity() {
-    private val fileName = "Food.txt"
+    private val fileName = "Food-Data.txt"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             val context = LocalContext.current
+            val foodItemsState = remember { mutableStateOf<List<FoodItem>>(emptyList()) }
+
             if (!fileExist(context, fileName)) {
                 fileCreate(context, fileName)
             }
-            Tab()
+            val foodItems = remember { mutableStateOf(fileRead(this, fileName)) }
+            Tab(foodItems.value, onAddItem = { newItem ->
+                val updatedItems = foodItems.value + newItem
+                foodItems.value = updatedItems
+                fileWrite(this, updatedItems, fileName)
+            })
+
         }
     }
 }
+
+private fun fileRead(context: Context, fileName: String): List<FoodItem> {
+    val file = File(context.filesDir, fileName)
+    val foodItems = mutableListOf<FoodItem>()
+    file.bufferedReader().useLines { lines ->
+        lines.forEach { line ->
+            val parts = line.split(",")
+            if (parts.size == 2) {
+                val name = parts[0]
+                val price = parts[1].toIntOrNull() ?: 0
+                foodItems.add(FoodItem(name, price))
+            }
+        }
+    }
+    return foodItems
+}
+data class FoodItem(val name: String, val price: Int, val type: String = "")
+
 private fun fileExist(context: Context, fileName: String): Boolean {
     val file = File(context.filesDir, fileName)
     return file.exists()
@@ -77,17 +104,107 @@ private fun fileCreate(context: Context, fileName: String) {
     val file = File(context.filesDir, fileName)
     file.createNewFile()
 }
+private fun fileWrite(context: Context, foodItems: List<FoodItem>, fileName: String) {
+    val file = File(context.filesDir, fileName)
+    file.bufferedWriter().use { writer ->
+        foodItems.forEach { item ->
+            writer.write("${item.name},${item.price}\n")
+        }
+    }
+}
+
+@Composable
+fun FoodList(item: FoodItem, isChecked: Boolean, onCheckedChange: (Boolean) -> Unit) {
+    Surface(
+        color = Color.White,
+        border = BorderStroke(2.dp, Color(0xFF74C931)),
+        modifier = Modifier.padding(vertical = 10.dp, horizontal = 20.dp)
+    ) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Checkbox(
+                checked = isChecked,
+                colors = CheckboxDefaults.colors(checkedColor = Color.Black),
+                onCheckedChange = onCheckedChange
+            )
+            Column(
+                modifier = Modifier
+                    .padding(horizontal = 10.dp, vertical = 15.dp)
+                    .fillMaxWidth()
+            ) {
+                Row(
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    modifier = Modifier.fillMaxWidth()
+                ) {
+                    Column(modifier = Modifier.padding(start = 5.dp)) {
+                        Text(
+                            text = item.name,
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(
+                                fontSize = 25.sp
+                            )
+                        )
+
+                    }
+                    Column {
+                        Text(
+                            modifier = Modifier.padding(end = 10.dp),
+                            text = "₹" + item.price.toString(),
+                            color = Color.Black,
+                            style = MaterialTheme.typography.headlineMedium.copy(
+                                 fontSize = 25.sp
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RecyclerView(names: List<FoodItem>, selectedItems: Set<FoodItem>, onItemCheckedChange: (FoodItem, Boolean) -> Unit) {
+    LazyColumn(modifier = Modifier.padding(vertical = 4.dp), verticalArrangement = Arrangement.Top) {
+        items(items = names) { item ->
+            FoodList(
+                item = item,
+                isChecked = selectedItems.contains(item),
+                onCheckedChange = { isChecked ->
+                    onItemCheckedChange(item, isChecked)
+                }
+            )
+        }
+    }
+}
 
 
 @OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun Tab(
-){
-    Column (modifier = Modifier.padding(PaddingValues(10.dp))){
+fun Tab(foodItems: List<FoodItem>, onAddItem: (FoodItem) -> Unit    ){
+    val context= LocalContext.current
+    val activity=context as? Activity
+    Column (){
         val pagerState = rememberPagerState (
             pageCount = { 2 }
         )
         val coroutineScope = rememberCoroutineScope()
+        Box(
+            modifier = Modifier
+                .background(color = Color(0xFF181818))
+                .fillMaxWidth(),
+        ){
+            Icon(imageVector = Icons.Outlined.Create,
+                contentDescription = null,
+                modifier = Modifier.padding(start = 15.dp).align(Alignment.CenterStart).size(25.dp), tint = Color.White)
+            Text(
+                text = "Billify",
+                modifier = Modifier.padding(top = 15.dp, bottom = 15.dp, start = 55.dp),
+                fontSize = 25.sp,
+                color = Color.White
+            )
+            Icon(imageVector = Icons.Filled.List,
+                contentDescription = null,
+                modifier = Modifier.padding(end = 15.dp).align(Alignment.CenterEnd).size(30.dp), tint = Color.White)
+        }
         TabRow(
             selectedTabIndex = pagerState.currentPage,
             Modifier.background(Color(0xFF74C931)),
@@ -97,6 +214,7 @@ fun Tab(
                 TabRowDefaults.Indicator(
                     modifier = Modifier.tabIndicatorOffset(tabPositions[pagerState.currentPage]),
                     height = 2.dp,
+                    color = Color(0xFF74C931)
                 )
             }
         ) {
@@ -148,16 +266,18 @@ fun Tab(
             userScrollEnabled = false
         ) {
             if (pagerState.currentPage==0){
-                FAB()
+                FoodDisplay(foodItems,onAddItem)
             }
         }
     }
 }
 
 @Composable
-fun FAB(){
+fun FoodDisplay(foodItems: List<FoodItem>, onAddItem: (FoodItem) -> Unit){
     val context = LocalContext.current
     val activity=context as? Activity
+    var selectedItems by remember { mutableStateOf(setOf<FoodItem>()) }
+    val totalPrice by remember { derivedStateOf { selectedItems.sumOf { it.price } } }
 
     Column(
         modifier = Modifier
@@ -166,7 +286,23 @@ fun FAB(){
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.Start,
     ) {
-
+        Text(
+            text = "Total Price: ₹ $totalPrice",
+            modifier = Modifier.padding(top = 30.dp, bottom = 10.dp, start = 25.dp),
+            fontSize = 20.sp,
+            color = Color.Black
+        )
+        RecyclerView(
+            names = foodItems,
+            selectedItems = selectedItems,
+            onItemCheckedChange = { item, isChecked ->
+                selectedItems = if (isChecked) {
+                    selectedItems + item
+                } else {
+                    selectedItems - item
+                }
+            }
+        )
         Box(modifier = Modifier
             .fillMaxSize()
             .padding(14.dp)) {
@@ -190,8 +326,9 @@ fun FAB(){
     }
 }
 
-@Preview(showBackground = true, showSystemUi = true)
+
+@Preview(showBackground = true,showSystemUi = true)
 @Composable
-fun MainPreview() {
-    Tab()
+fun GreetingPreview() {
+    Tab(foodItems = emptyList(), onAddItem = {})
 }
